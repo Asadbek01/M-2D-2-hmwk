5381fae1c6c76fee3822723ffb7e37a52fcde766
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-10-05 17:01:57 +0200

    updated

---
+++ index.js
@@ -0,0 +1,25 @@
+// function header(title){
+//     console.log(`-------==[${title.toUpperCase()}]==--------`)
+// }
+// header("1 st Answer")
+
+// let array = ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
+// console.log(array)
+// header ("2st Answer")
+  
+// var stringArray = ["Home","About","Contact"];     
+
+// function buildList(){
+//     var newList = document.createElement("ul");
+//     var newListItem;
+//     document.getElementById("listItem").appendChild(newList);
+
+//     for( var i = 0; i < stringArray.length; i++){
+//         newListItem = document.createElement('li');
+//         newListItem.textContent = stringArray[i];
+//         newList.appendChild(newListItem);
+//     }
+// }
+
+// buildList();
+
